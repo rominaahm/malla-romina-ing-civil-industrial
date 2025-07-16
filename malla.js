0fca8119b077f30a99ce6781c101870633fff713
rominaahm <romihm2005@gmail.com>
2025-07-16 16:39:48 -0400

    Add files via upload

--- malla.js
+++ malla.js
@@ -2,24 +2,19 @@
 const ramos = [
   { codigo: "ICI101", nombre: "Álgebra I", semestre: 1 },
   { codigo: "ICI102", nombre: "Fund. Química", semestre: 1 },
-  { codigo: "ICI103", nombre: "Herr. Computacionales", semestre: 1 },
-  { codigo: "ICI104", nombre: "Inglés I", semestre: 1 },
-  { codigo: "ICI105", nombre: "CFG", semestre: 1 },
   { codigo: "ICI201", nombre: "Álgebra II", semestre: 2, prerequisitos: ["ICI101"] },
   { codigo: "ICI202", nombre: "Cálculo I", semestre: 2 },
-  { codigo: "ICI203", nombre: "Intro Programación", semestre: 2 },
-  { codigo: "ICI204", nombre: "Inglés II", semestre: 2 },
-  { codigo: "ICI205", nombre: "CFG", semestre: 2 },
-  { codigo: "ICI300", nombre: "Electricidad y Magnetismo", semestre: 3 },
-  { codigo: "ICI301", nombre: "Prob. y Estadística", semestre: 3, prerequisitos: ["ICI101"] },
+  { codigo: "ICI301", nombre: "Probabilidades", semestre: 3, prerequisitos: ["ICI101"] },
   { codigo: "ICI302", nombre: "Cálculo II", semestre: 3, prerequisitos: ["ICI202"] },
-  { codigo: "ICI303", nombre: "BD e Información", semestre: 3 },
-  { codigo: "ICI304", nombre: "Inglés III", semestre: 3 },
   { codigo: "ICI400", nombre: "Cálculo III", semestre: 4, prerequisitos: ["ICI302"] },
   { codigo: "ICI401", nombre: "Ecuaciones Diferenciales", semestre: 4 },
-  { codigo: "ICI402", nombre: "Termodinámica", semestre: 4 },
-  { codigo: "ICI403", nombre: "Taller de BD", semestre: 4 },
-  { codigo: "ICI404", nombre: "Inglés IV", semestre: 4 }
+  { codigo: "ICI501", nombre: "IO I", semestre: 5, prerequisitos: ["ICI301"] },
+  { codigo: "ICI502", nombre: "Modelos Estocásticos", semestre: 5, prerequisitos: ["ICI301"] },
+  { codigo: "ICI602", nombre: "Econometría", semestre: 6, prerequisitos: ["ICI301"] },
+  { codigo: "ICI703", nombre: "Gestión Operaciones", semestre: 7, prerequisitos: ["ICI501"] },
+  { codigo: "ICI805", nombre: "Finanzas", semestre: 8, prerequisitos: ["ICI705"] },
+  { codigo: "ICI906", nombre: "Liderazgo para la Gestión", semestre: 9, prerequisitos: ["ICI804"] },
+  { codigo: "ICI006", nombre: "Seminario de Título", semestre: 11, prerequisitos: ["ICI003"] }
 ];
 
 function crearMalla() {
