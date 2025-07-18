fca7b07827b5699f89dcf972e5b74f44af90cc56
rominaahm <romihm2005@gmail.com>
2025-07-18 13:09:52 -0400

    Add files via upload

--- malla.js
+++ malla.js
@@ -1,21 +1,6 @@
-
 const ramos = [
-  { codigo: "ICI101", nombre: "Álgebra I", semestre: 1 },
-  { codigo: "ICI102", nombre: "Fund. Química", semestre: 1 },
-  { codigo: "ICI201", nombre: "Álgebra II", semestre: 2, prerequisitos: ["ICI101"] },
-  { codigo: "ICI202", nombre: "Cálculo I", semestre: 2 },
-  { codigo: "ICI301", nombre: "Probabilidades", semestre: 3, prerequisitos: ["ICI101"] },
-  { codigo: "ICI302", nombre: "Cálculo II", semestre: 3, prerequisitos: ["ICI202"] },
-  { codigo: "ICI400", nombre: "Cálculo III", semestre: 4, prerequisitos: ["ICI302"] },
-  { codigo: "ICI401", nombre: "Ecuaciones Diferenciales", semestre: 4 },
-  { codigo: "ICI501", nombre: "IO I", semestre: 5, prerequisitos: ["ICI301"] },
-  { codigo: "ICI502", nombre: "Modelos Estocásticos", semestre: 5, prerequisitos: ["ICI301"] },
-  { codigo: "ICI602", nombre: "Econometría", semestre: 6, prerequisitos: ["ICI301"] },
-  { codigo: "ICI703", nombre: "Gestión Operaciones", semestre: 7, prerequisitos: ["ICI501"] },
-  { codigo: "ICI805", nombre: "Finanzas", semestre: 8, prerequisitos: ["ICI705"] },
-  { codigo: "ICI906", nombre: "Liderazgo para la Gestión", semestre: 9, prerequisitos: ["ICI804"] },
-  { codigo: "ICI006", nombre: "Seminario de Título", semestre: 11, prerequisitos: ["ICI003"] }
 ];
+
 
 function crearMalla() {
   const container = document.getElementById("malla-container");
